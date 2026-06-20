2aee71d09b25b77d26b74c7a8427a7fe194fad3a
unknown <oluchibenard3@gmail.com>
2026-06-20 18:48:04 +0100

    add the code just written now

---
+++ lib/features/home/widgets/featured_schools_banner.dart
@@ -0,0 +1,20 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../core/constants/app_colors.dart';
+import '../../../core/constants/app_text_styles.dart';
+
+class FeaturedSchoolsBanner extends StatefulWidget {
+  const FeaturedSchoolsBanner({super.key});
+
+  @override
+  State<FeaturedSchoolsBanner> createState() => _FeaturedSchoolsBannerState();
+}
+
+class _FeaturedSchoolsBannerState extends State<FeaturedSchoolsBanner> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
