420f7fde999762d7af70caa77a0eba905fc48a11
unknown <oluchibenard3@gmail.com>
2026-06-21 00:39:53 +0100

    all the codes written so far

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
 import '../../core/constants/app_colors.dart';
 import '../../core/constants/app_text_styles.dart';
+import '../../providers/auth_provider.dart';
+import 'widgets/featured_schools_banner.dart';
+import 'widgets/application_summary_card.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final authProvider = context.watch<AuthProvider>();
+    final String firstName = authProvider.userProfile?['fullName']
+    ?.toString()
+    .split(' ')
+    .first ??
+    'Student';
+
     return Scaffold(
       backgroundColor: AppColors.background,
       appBar: AppBar(
---
+++ lib/features/home/widgets/application_summary_card.dart
@@ -0,0 +1,71 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import '../../../core/constants/app_text_styles.dart';
+import '../../../core/constants/app_colors.dart';
+
+class ApplicationSummaryCard extends StatelessWidget {
+  final String count;
+  final String label;
+  final Color color;
+  final IconData icon;
+
+  const ApplicationSummaryCard({
+    super.key,
+    required this.count,
+    required this.label,
+    required this.color,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16.w),
+      decoration: BoxDecoration(
+        color: color.withOpacity(0.08),
+        borderRadius: BorderRadius.circular(16.r),
+        border: Border.all(
+          color: color.withOpacity(0.2),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          //Icon
+          Container(
+            width: 36.w,
+              height: 36.w,
+            decoration: BoxDecoration(
+              color: color.withOpacity(0.15),
+              borderRadius: BorderRadius.circular(10.r),
+            ),
+            child: Icon(
+              icon,
+              color: color,
+                size: 20.w,
+            ),
+          ),
+          SizedBox(height: 12.h),
+
+          // Count
+          Text(
+            count,
+            style: AppTextStyles.displayMedium.copyWith(
+              color: color,
+              fontSize: 28,
+            ),
+          ),
+          SizedBox(height: 4.h),
+
+          // Label
+          Text(
+            label,
+            style: AppTextStyles.bodySmall.copyWith(
+              color: AppColors.textSecondary,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
