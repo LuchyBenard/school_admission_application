7b85533abc67c4e003ced9dc55524eb59a6c8595
unknown <oluchibenard3@gmail.com>
2026-06-23 10:58:26 +0100

    the codes written today

--- lib/features/home/widgets/featured_schools_banner.dart
+++ lib/features/home/widgets/featured_schools_banner.dart
@@ -12,10 +12,63 @@ class FeaturedSchoolsBanner extends StatefulWidget {
 }
 
 class _FeaturedSchoolsBannerState extends State<FeaturedSchoolsBanner> {
+  int _currentIndex = 0;
+
+  // Placeholder data - we will replace with firestore data later
+  final List<Map<String, String>> _featuredSchools = [
+    {
+      'name': 'University of Lagos',
+      'location': 'Lagos, Nigeria',
+      'tag': 'Top Ranked',
+    },
+    {
+      'name': 'University of Nigeria, Nsukka',
+      'location': 'Enugu, Nigeria',
+      'tag': 'Top Ranked',
+    },
+    {
+      'name': 'University of Ibadan',
+      'location': 'Ibadan, Nigeria',
+      'tag': 'First in Nigeria',
+    },
+    {
+      'name': 'Ahmadu Bello University',
+      'location': 'Zaria, Nigeria',
+      'tag': 'Federal University',
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     // COMING BACK TO FINISH YOU UP
-    return const Placeholder();
+    return Column(
+      children: [
+        CarouselSlider.builder(
+            itemCount: _featuredSchools.length,
+            itemBuilder: (context, index, realIndex){
+              final school = _featuredSchools[index];
+              return _buildBannerCard(school);
+            },
+          options: CarouselOptions(
+            height: 160.h,
+            viewportFraction: 0.92
+              enlargeCenterPage: true,
+            autoPlay: true,
+            autoPlayInterval: Duration(seconds: 4),
+            autoPlayCurve: Curves.easeInOut,
+            onPageChanged: (index, reason) {
+              setState(() => _currentIndex = index);
+            }
+          ),
+        ),
+
+        SizedBox(height: 12.h),
+
+        // DOT INDICATORS
+
+      ],
+
+    );
   }
 }
 
