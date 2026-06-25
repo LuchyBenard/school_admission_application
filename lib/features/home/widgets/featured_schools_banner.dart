71026c94235819754d403403d355fa6f8b1acc35
unknown <oluchibenard3@gmail.com>
2026-06-25 13:58:58 +0100

    all the codes written

--- lib/features/home/widgets/featured_schools_banner.dart
+++ lib/features/home/widgets/featured_schools_banner.dart
@@ -47,18 +47,7 @@ class _FeaturedSchoolsBannerState extends State<FeaturedSchoolsBanner> {
             itemCount: _featuredSchools.length,
             itemBuilder: (context, index, realIndex){
               final school = _featuredSchools[index];
-              // return _buildBannerCard(school);
-              return Container(
-                width: double.infinity,
-                height: 100.h,
-                decoration: BoxDecoration(
-                  color: AppColors.surface,
-                  borderRadius: BorderRadius.circular(12.r),
-                  border: Border.all(
-                    color: AppColors.border,
-                  ),
-                ),
-              );
+               return _buildBannerCard(school);
             },
           options: CarouselOptions(
             height: 160.h,
@@ -69,16 +58,111 @@ class _FeaturedSchoolsBannerState extends State<FeaturedSchoolsBanner> {
             autoPlayCurve: Curves.easeInOut,
             onPageChanged: (index, reason) {
               setState(() => _currentIndex = index);
-            }
+            },
           ),
         ),
 
         SizedBox(height: 12.h),
 
         // DOT INDICATORS
-
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(
+            _featuredSchools.length,
+              (index) => AnimatedContainer(
+                  duration: Duration(milliseconds: 300),
+                margin: EdgeInsets.symmetric(horizontal: 3.w),
+                width: _currentIndex == index ? 20.w : 6.w,
+                height: 6.h,
+                decoration: BoxDecoration(
+                  color: _currentIndex == index
+                      ? AppColors.primary
+                      : AppColors.border,
+                  borderRadius: BorderRadius.circular(3.r),
+                ),
+              ),
+          ),
+        ),
       ],
+    );
+  }
 
+  Widget _buildBannerCard(Map<String, String> school) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.symmetric(horizontal: 4.w),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            AppColors.primary,
+            AppColors.primaryDark,
+          ],
+        ),
+        borderRadius: BorderRadius.circular(16.r),
+        boxShadow: [
+          BoxShadow(
+            color: AppColors.primary.withOpacity(0.3),
+            blurRadius: 12,
+            offset: Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Padding(
+          padding: EdgeInsets.all(20.w),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            //Tag
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 4.h),
+              decoration: BoxDecoration(
+                color: AppColors.background.withOpacity(0.2),
+                borderRadius: BorderRadius.circular(20.r),
+              ),
+              child: Text(
+                school['tag']!,
+                style: AppTextStyles.caption.copyWith(
+                  color: AppColors.background,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+
+            // School Info
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  school['name']!,
+                  style: AppTextStyles.h2.copyWith(
+                    color: AppColors.background,
+                  ),
+                ),
+                SizedBox(height: 4.h),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.location_on_outlined,
+                      color: AppColors.background.withOpacity(0.8),
+                      size: 14.w,
+                    ),
+                    SizedBox(width: 4.w),
+                    Text(
+                      school['location']!,
+                          style: AppTextStyles.bodySmall.copyWith(
+                        color: AppColors.background.withOpacity(0.8),
+                    ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
