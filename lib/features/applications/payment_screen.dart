b076b385ba091dc2c9a9e4cbfa2f6489112d4126
unknown <oluchibenard3@gmail.com>
2026-07-17 15:57:00 +0100

    the payment screen codes

---
+++ lib/features/applications/payment_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_scrennutil.dart';
+import '../../core/constatnts/app_colors.dart';
+import '../../core/constants/app_text_styles.dart';
+
+class PaymentScreen extends StatelessWidget {
+  const PaymentScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
