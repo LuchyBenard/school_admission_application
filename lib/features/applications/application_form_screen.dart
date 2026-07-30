9b64e32f8495c8ce11654121b7e17846d1a7b86e
unknown <oluchibenard3@gmail.com>
2026-07-30 17:18:43 +0100

    the codes of today

--- lib/features/applications/application_form_screen.dart
+++ lib/features/applications/application_form_screen.dart
@@ -576,6 +576,13 @@ class _ApplicationFormScreenState extends State<ApplicationFormScreen> {
                   : Text(
                       _currentStep < _totalSteps - 1 ? 'Next' : 'Submit Application',
                     ),
+              if (applicationId != null) {
+                Navigator.pushNamed(
+                context,
+                '/document-upload',
+          arguments: applicationId,
+                );
+          }
             ),
           ),
         );
